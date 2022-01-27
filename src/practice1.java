8b02d9e76a60309f9eea1991fc6eb233c727f59a
Ranosh <raldarra@gmail.com>
2022-01-27 13:41:08 -0500

    Day3 practice, Step3 added

--- src/practice1.java
+++ src/practice1.java
@@ -2,5 +2,8 @@ public class practice1 {
     public static void main(String[] args) {
         System.out.println("Step1");
         System.out.println("Step2");
+        System.out.println("Step3");
+        System.out.println("Step4");
+        System.out.println("Step5");
     }
 }
